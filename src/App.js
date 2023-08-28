fa647ba7ffca5afae8202194a68c393a9054ec8b
just-osaze <osaigbevo.osaz@gmail.com>
2023-08-28 14:45:32 +0100

    Imported Routes, Route path, & Countries and Country components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 
+import Countries from "./components/Countries";
+import Country from "./components/Country";
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Countries />} />
+        <Route path='/:countryName' element={<Country />} />
+      </Routes>
     </div>
   );
 }
