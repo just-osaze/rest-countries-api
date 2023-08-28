aa8408f94472bc84c4d0e972da006ed5143d3bde
just-osaze <osaigbevo.osaz@gmail.com>
2023-08-28 14:43:31 +0100

    coded the Countries.jsx component

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import data from '../data.json';
+import { Link } from 'react-router-dom';
+
+
 
 function Countries() {
   return (
-    <div>Countries</div>
+    <div className='grid grid-cols-4 gap-14 px-20'>
+      {data.map((country, index) => {
+        return (
+          <div key={index} className='w-64 bg-white border-md border-gray-200 rounded-lg shadow'>
+           
+           <Link to={`/${country.name}`}>
+              <img className='bg-cover h-36 max-w-full object-cover ' src={country.flags.svg} alt={data.flag} />
+              
+              <div className='m-6'>
+                <h1 className='mb-3 text-base'><b>{country.name}</b></h1>
+                <h1 className='text-sm'><b>Population: </b>{country.population}</h1>
+                <h1 className='text-sm'><b>Region: </b>{country.region}</h1>
+                <h1 className='text-sm'><b>Capital: </b>{country.capital}</h1>
+              </div>
+           </Link>
+           
+            
+          </div>
+          
+        )
+      })}
+    </div>
   )
 }
 
-export default Countries
+export default Countries;
